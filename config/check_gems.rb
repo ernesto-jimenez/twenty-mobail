785d0f536f03f7785dc8bbcebe461c9744ab6163
Ernesto Jiménez <ernesto.jimenez@negonation.com>
2009-03-15 19:39:38 +0100

    commited check_gems.rb and constants.rb

---
+++ config/check_gems.rb
@@ -0,0 +1,37 @@
+GEMS = {
+  'mechanize' => ">= 0.9.2",
+  'gmailer'   => "0.2.1",
+  'daemons'   => "1.0.10"
+}
+
+def try_gem(gem_name, version, print=true)
+  print "Checking #{gem_name}: " if print
+  begin
+    gem gem_name, version
+  rescue Exception
+    puts "FAIL" if print
+    return false
+  end
+  puts "OK" if print
+  return true
+end
+
+begin
+  require 'rubygems'
+rescue Exception
+  STDERR.puts "You need to install rubygems.\ncheck http://docs.rubygems.org/"
+  exit(false)
+end
+
+pending_to_install = []
+GEMS.each do |gem_name, version|
+  pending_to_install << gem_name unless try_gem(gem_name, version)
+end
+
+puts ""
+if pending_to_install.empty?
+  puts "Everything OK. Your environment is ready"
+else
+  STDERR.puts "You need to install some gems, type in your terminal:"
+  STDERR.puts "sudo gem install #{pending_to_install.join(' ')}"
+end
