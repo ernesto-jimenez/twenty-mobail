140b823333a4d496eb8bcd4208b01c7abb560abb
Ernesto Jiménez <ernesto.jimenez@negonation.com>
2009-03-15 20:03:06 +0100

    updated gem requirements

--- config/check_gems.rb
+++ config/check_gems.rb
@@ -1,11 +1,14 @@
 GEMS = {
-  'mechanize' => ">= 0.9.2",
-  'gmailer'   => "0.2.1",
-  'daemons'   => "1.0.10"
+  'mechanize'    => ">= 0.9.2",
+  'gmailer'      => "0.2.1",
+  'daemons'      => "1.0.10",
+  'sqlite3-ruby' => "1.2.4",
+  'nokogiri'     => "1.2.2",
+  'activerecord' => "> 2.0"
 }
 
 def try_gem(gem_name, version, print=true)
-  print "Checking #{gem_name}: " if print
+  print "Checking #{gem_name} version #{version}: " if print
   begin
     gem gem_name, version
   rescue Exception
@@ -25,7 +28,7 @@ end
 
 pending_to_install = []
 GEMS.each do |gem_name, version|
-  pending_to_install << gem_name unless try_gem(gem_name, version)
+  pending_to_install << "sudo gem install #{gem_name} -v \"#{version}\"" unless try_gem(gem_name, version)
 end
 
 puts ""
@@ -33,5 +36,7 @@ if pending_to_install.empty?
   puts "Everything OK. Your environment is ready"
 else
   STDERR.puts "You need to install some gems, type in your terminal:"
-  STDERR.puts "sudo gem install #{pending_to_install.join(' ')}"
+  pending_to_install.each do |msg|
+    STDERR.puts msg
+  end
 end
