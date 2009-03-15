ac0e2dd70b99a1990b4b2adc95cefe216d0911c2
Ernesto Jiménez <ernesto.jimenez@negonation.com>
2009-03-15 20:04:36 +0100

    added tuent_credential sqlite3 store

---
+++ lib/tuenti_credential.rb
@@ -0,0 +1,30 @@
+require 'base'
+require 'sqlite3'
+require 'activerecord'
+
+# connect to database.  This will create one if it doesn't exist
+MY_DB_NAME = "tuenti_credentials.db"
+MY_DB = SQLite3::Database.new(MY_DB_NAME)
+
+# get active record set up
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => MY_DB_NAME)
+
+# create your AR class
+class TuentiCredential < ActiveRecord::Base
+  validates_presence_of :phone
+  validates_uniqueness_of :phone
+  
+  validates_presence_of :email
+  validates_uniqueness_of :email, :scope => :phone
+  
+  validates_presence_of :password
+end
+
+# do a quick pseudo migration.  This should only get executed on the first run
+if !TuentiCredential.table_exists?
+  ActiveRecord::Base.connection.create_table(:tuenti_credentials) do |t|
+    t.column :phone, :string
+    t.column :email, :string
+    t.column :password, :string
+  end
+end
