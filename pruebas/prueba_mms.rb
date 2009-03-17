8a1c3afb81e5488b7aa89af854ea84169285e7b5
Ernesto Jiménez <ernesto.jimenez@negonation.com>
2009-03-17 22:07:09 +0100

    removing dummy tests

--- pruebas/prueba_mms.rb
+++
@@ -1,4 +0,0 @@
-require 'lib/open_movilforum'
-
-wa = OpenMovilforum::MMS::Receiver::Movistar.new
-wa.run_once
--- pruebas/prueba_tuenti.rb
+++
@@ -1,8 +0,0 @@
-require 'base'
-require 'tuenti'
-
-t = Tuenti.new(SMS_RECEIVER_MAIL, SMS_RECEIVER_PASS)
-t.set_status("Cambiando estado #{Time.now.to_s}")
-#t.upload_img("/Users/hydrus/Pictures/Imagen 3.png")
-#t.upload_img("/Users/hydrus/Pictures/saturno.jpg")
-#t.upload_img("/Users/hydrus/Pictures/Ichigo_by_JojoRatonLaveur.png")
